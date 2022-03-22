23bd0602e0df28e422492765be93ff844e1366a6
nxjuan <juannogueiraxavier@gmail.com>
2022-03-22 14:59:35 -0300

    22.03.2022
    aulas novas

--- trabalho-danilo(29.03)/imc.js
+++ trabalho-danilo(29.03)/imc.js
@@ -4,17 +4,17 @@ function calculate(){
 
     var imc=peso/(altura**2)
     if(imc<18.5){
-        window.alert("Você Apresenta um Grau de Desnutrição")
+        window.alert(imc + "Você Apresenta um Grau de Desnutrição")
     }else if(imc<24.9){
-        window.alert("Você Está na Faixa de Peso Ideal")
+        window.alert(imc + "Você Está na Faixa de Peso Ideal")
     }
     else if(imc<29.9){
-        window.alert("Você está Acima do Peso Ideal")
+        window.alert(imc + "Você está Acima do Peso Ideal")
     }
     else if(imc<39.9){
-        window.alert("Você Apresenta um Grau de Obesidade")
+        window.alert(imc + "Você Apresenta um Grau de Obesidade")
     }
     else if(imc>39.9){
-        window.alert("Você Apresenta Obesidade Mórbida")
+        window.alert(imc + "Você Apresenta Obesidade Mórbida")
     }
 }
